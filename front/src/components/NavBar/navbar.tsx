b491efb1c0767525bbea6333a64af9b9934f0f97
heyossam <heyossam11@gmail.com>
2024-09-28 15:50:05 +0500

    metamask auth added

--- front/src/components/NavBar/navbar.tsx
+++ front/src/components/NavBar/navbar.tsx
@@ -1,7 +1,50 @@
-import { User } from 'lucide-react';
+import { useState, useEffect } from 'react';
+import { User, LogIn, LogOut, ShoppingCart, Wallet, Coins } from 'lucide-react';
 import { ethers } from 'ethers';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export default function NavBar(){
+    const [account, setAccount] = useState<string | null>(null);
+
+    const connectWallet = async () => {
+        if((window as any).ethereum){
+            try{
+                const accounts = await (window as any).ethereum.request({
+                    method:"eth_requestAccounts",
+                });
+                setAccount(accounts[0]);
+            } catch(error) {
+                console.error("Error connecting to MetaMask:", error);
+            }
+        } else {
+            alert("Please install MetaMask");
+        }
+    };
+
+    const checkWalletConnection = async () => {
+        if((window as any).ethereum){
+            const accounts = await (window as any).ethereum.request({ method: 'eth_accounts' });
+            if (accounts.length > 0) {
+              setAccount(accounts[0]);
+            }
+        }
+    };
+
+    const logoutWallet = () => {
+        setAccount(null);
+    }
+
+    useEffect(() => {
+        checkWalletConnection();
+    }, []);
+
     return (
         <div className="flex justify-between">
             <div>
@@ -11,7 +54,36 @@ export default function NavBar(){
                 MarketPlace</h1>
             </div>
             <div>
-                <button><User/></button>
+                {account ? (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger><User/></DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                          <DropdownMenuLabel>{account}</DropdownMenuLabel>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>
+                              <Wallet className="mr-2 h-4 w-4" />
+                              <span>Balance: 1 ETH </span>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem>
+                              <ShoppingCart className="mr-2 h-4 w-4" />
+                              <span>Create Listing</span>
+                          </DropdownMenuItem>
+                          <DropdownMenuItem>
+                              <Coins className="mr-2 h-4 w-4" />
+                              <span>Withdraw</span>
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem onSelect={logoutWallet}>
+                              <LogOut className="mr-2 h-4 w-4" />
+                              <span>Log out</span>
+                          </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                ) : (
+                    <div>
+                        <button onClick={connectWallet}><LogIn /></button>
+                    </div>
+                )} 
             </div>
         </div>
     );
